78b224948c5d907700baa5c4bb6fb40c38a2e0eb
yiminghe <yiminghe@gmail.com>
2013-12-24 22:51:47 +0800

    fix index

--- index.php
+++ index.php
@@ -9,18 +9,6 @@
           echo "src='http://g.tbcdn.cn/kissy/k/1.4.1/??seed-min.js,import-style-min.js' data-config='{combine:true}' ";
       }
       ?>></script>
-    <?php 
-      if(!isset($_GET['dev'])){
-          echo "<script src='build/my/modules.js'></script>";
-          echo "<script>KISSY.importStyle('my/index');</script>";
-      }
-    ?>
-</head>
-<body>
-    <p>a: <input id='a'/></p>
-    <p>b: <input id='b'/></p>
-    <p>result: <span id='c'></span></p>
-    <p><button id='add'>add</button></p>
     <script>
         KISSY.config({
             packages:[{
@@ -35,6 +23,20 @@
               
             }]
         });
+    </script>  
+    <?php 
+      if(!isset($_GET['dev'])){
+          echo "<script src='build/my/modules.js'></script>";
+          echo "<script>KISSY.importStyle('my/index');</script>";
+      }
+    ?>
+</head>
+<body>
+    <p>a: <input id='a'/></p>
+    <p>b: <input id='b'/></p>
+    <p>result: <span id='c'></span></p>
+    <p><button id='add'>add</button></p>
+    <script>
         KISSY.use('my/index',function(S, my){
             my.init(document.getElementById('a'),document.getElementById('b'),document.getElementById('c'),document.getElementById('add'));
         });
